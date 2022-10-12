592ddbbd9510bf45977a0e8d7f63a16cfb2872d5
k.roman <romaknyazev55@yandex.ru>
2022-10-12 15:20:31 +0300

    1

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import sys
 from PyQt5 import QtWidgets
 from qt_material import apply_stylesheet
 from ui.main_window import MainWindow
+from PyQt5.QtWidgets import QApplication
+from PyQt5 import QtCore
+
+
+if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+    QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+
+if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+    QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
 
 app = QtWidgets.QApplication(sys.argv)
 window = MainWindow()
 apply_stylesheet(app, theme='dark_amber.xml')
-window.showMaximized()
+window.show()
 app.exec_()
